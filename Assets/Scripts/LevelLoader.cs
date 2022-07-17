1a25d36aea98777291be36b48ab8ec12751c922a
Katja Mathesius <67726169+katmathematics@users.noreply.github.com>
2022-07-16 21:16:28 -0400

    Audio Updates and Enemy Sprite
    Added audio for the win/loss screens and a custom sprite for the enemy

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -7,16 +7,22 @@ public class LevelLoader : MonoBehaviour
 {
     public Animator transition;
 
+    //public int levelIndex;
+
     public float transitionTime = 1;
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void LoadNextLevel() {
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void LoadSpecificLevel(int levelIndex) {
+        StartCoroutine(LoadLevel(levelIndex));
     }
 
     IEnumerator LoadLevel(int levelIndex) {
