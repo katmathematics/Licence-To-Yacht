ffe60fc74d023064b4c4a3cd53a850d9d3641567
Katja Mathesius <67726169+katmathematics@users.noreply.github.com>
2022-07-27 22:55:56 -0400

    UI Update
    - Corrected the scaling mode on the UI
    - Added a menu screen
    - Added a menu screen button to quit the game
    - New UI Assets (WIP)
    - Changed James Yond's name the James Yacht
    - Changed License to Licence
    - Changed various other game text

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    public string unitName;
+    public string unitName; //Name of the unit the script represents
 
-    public int damage;
-    public int maxHP;
-    public int currentHP;
-    public int money;
+    public int damage; //The unit's attack damage 
+    public int maxHP; //The unit's maximum hit points
+    public int currentHP; //The unit's current hit points
+    public int money; //The unit's current money
 
+    //Function for taking damage
     public bool TakeDamage(int dmg) {
         currentHP -= dmg;
 
@@ -20,5 +21,21 @@ public class Unit : MonoBehaviour
         else {
             return false;
         }
+    }
+
+    public void AddMoney(int new_money) {
+        money += new_money;
+    }
+
+    public void RemoveMoney(int lost_money) {
+        money -= lost_money;
+    }
+
+    public void UpdateName(string new_name) {
+        unitName = new_name;
+    }
+
+    public void UpdateMaxHP(int new_max_hp) {
+        maxHP = new_max_hp;
     }
 }
